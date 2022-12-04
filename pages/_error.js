ebaab76ce39548f7195b609f829ad74f2a88dec1
Olierook <robbert_olierook@hotmail.com>
2022-12-04 17:48:22 +0100

    remove lf

--- pages/_error.js
+++ pages/_error.js
@@ -1,9 +1,9 @@
 import Router from "next/router";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 export default function _error() {
   useEffect(() => {
     Router.push("/");
-  },[])
+  },[]);
   return <div />;
 }
